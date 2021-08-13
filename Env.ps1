a9f64d1c241c1b6823fd7845e84012ee28841250
Kevin Wells <kev.wells@gmail.com>
2021-08-13 10:20:43 -0700

    Delete Env.ps1

--- Env.ps1
+++
@@ -1,4 +0,0 @@
-$domain =           "mydomain.com" # Name of domain containing computers, used to temporarily add as trusted host for PSSession
-$remoteDir =        "c:\remoteDir" # Writeable remote directory full path where files will be temporarily uploaded
-$localDataFolder =  "RemoteBatchCmd" # Name of writeable local directory where reports/logs/files will be saved and/or uploaded from
-$localDataPath =    [Environment]::GetFolderPath("MyDocuments") + "\" + $localDataFolder # Full local path in MyDocuments
