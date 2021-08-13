3fe873d2a647db97767600d92f1a42a6d3250303
Kevin Wells <kev.wells@gmail.com>
2021-08-13 08:31:09 -0700

    Update Env.ps1

--- Env.ps1
+++ Env.ps1
@@ -1,4 +1,4 @@
-$domain =           "domain" # Name of domain containing computers, used to temporarily add as trusted host for PSSession
-$remoteDir =        "remoteDir" # Writeable remote directory full path where files will be temporarily uploaded
+$domain =           "mydomain.com" # Name of domain containing computers, used to temporarily add as trusted host for PSSession
+$remoteDir =        "c:\remoteDir" # Writeable remote directory full path where files will be temporarily uploaded
 $localDataFolder =  "\RemoteBatchCmd" # Name of writeable local directory where reports/logs/files will be saved and/or uploaded from
 $localDataPath =    [Environment]::GetFolderPath("MyDocuments") + "\RemoteBatchCmd" # Full local path
